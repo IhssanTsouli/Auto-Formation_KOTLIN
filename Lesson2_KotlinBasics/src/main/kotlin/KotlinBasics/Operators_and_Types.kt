ef914f39ef2f6f4a651c8c754044a4ebbc2dd51b
IhssanTsouli <ihssantsouli@gmail.com>
2023-02-06 12:48:13 +0100

    Hello world

---
+++ Lesson2_KotlinBasics/src/main/kotlin/KotlinBasics/Operators_and_Types.kt
@@ -0,0 +1,28 @@
+package KotlinBasics
+fun main (){
+    println("hello Ihssan")
+    val i: Int =6
+    val b1 = i.toByte()
+    println(b1)
+
+    val b2 :Byte = 1
+    val i1 : Int = b2.toInt()
+    println(i1)
+
+    var fish = 1
+    fish = 2
+    println(fish)
+
+    val age = 22
+    val firstname = "Ihssan"
+    val lastname = "Tsouli"
+    val a = 5
+    val b = 6
+
+    println("your name: $firstname your age : $age years old")
+
+    println("you are : ${firstname+lastname}")
+
+    println("la somme est : ${a+b} ")
+
+}
