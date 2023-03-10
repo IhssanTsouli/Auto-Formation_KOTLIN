0bab6593b380acf97881111fab5fb8cf3163a0a0
IhssanTsouli <ihssantsouli@gmail.com>
2023-03-10 12:48:30 +0100

    prototype basic

--- RecyclerView/app/src/main/java/com/example/recyclerview/TodoAdabter.kt
+++ RecyclerView/app/src/main/java/com/example/recyclerview/TodoAdabter.kt
@@ -3,24 +3,34 @@ package com.example.recyclerview
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import com.example.recyclerview.databinding.RecyclerRowBinding
+import kotlin.coroutines.jvm.internal.CompletedContinuation.context
 
 class CustomAdapter(val userList : ArrayList<data>): RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
 
 
-    override fun onCreateViewHolder(parent: ViewGroup, p1: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, p1: Int): RecyclerView.ViewHolder {
         val v = LayoutInflater.from(parent.context)
         val binding = RecyclerRowBinding.inflate(v, parent, false)
-        return ViewHolder(binding)
+        val deleteButton: Button = view.findViewById(R.id.btn_delete)
+        return RecyclerView.ViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
         return userList.size
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val data: data = userList[position]
         holder.bind(data)
+
+        holder.btn_delete.setOnClickListener {
+            data.userList.removeAt(position)
+            this.notifyItemRemoved(position)
+            /*Toast.makeText(context, "Task Deleted", Toast.LENGTH_SHORT).show()*/
+        }
     }
 
     class ViewHolder(private val binding: RecyclerRowBinding) : RecyclerView.ViewHolder(binding.root) {
---
+++ prototype_basic/app/src/main/java/com/example/prototype_basic/Data.kt
@@ -0,0 +1,4 @@
+package com.example.prototype_basic
+
+class Data {
+}
